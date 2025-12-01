48445daa6ab6ff5f0bf14a99b0fa904315fe35ed
titovaae <miya.dark@bk.ru>
2025-12-02 01:06:12 +0500

    asfsf

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -7,6 +7,30 @@ public class Point {
     int x;
     int y;
 
+    public Point(){
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public void flip(){
+        int temp = x * (-1);
+        x = (int) y * (-1);
+        y = temp;
+    }
+
+    public double distance(Point point){
+        return Math.sqrt(Math.pow(x - point.x, 2) + Math.pow(y - point.y, 2));
+    }
+
+    public String toString(){
+        return String.format("(%d, %d)", x, y);
+    }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
